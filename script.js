399c6154163dcb85118e88fcf0bf887bd1a76461
Steve S <catholickiwi2016@gmail.com>
2021-02-22 21:10:16 +1300

    write function to fizz buzz numbers to 100

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
 function listNumbers(){
     for(var i=1; i<100; i++){
-        console.log(i);
+        if (i % 3 == 0){ 
+            console.log("fizz");       
+            } else if(i % 5 == 0){            
+            console.log("buzz");            
+        }else{
+            console.log(i);
+        }       
     }
 }
 
